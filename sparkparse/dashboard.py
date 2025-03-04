500f858706781275beddca1ed01e32ad45bffd2e
Jeff Brennan <jeffbrennan10@gmail.com>
2025-03-04 16:49:06 -0500

    add basic dashboard resources

---
+++ sparkparse/dashboard.py
@@ -0,0 +1,165 @@
+import dash
+import dash_bootstrap_components as dbc
+import polars as pl
+from dash import Input, Output, State, callback, dcc, html
+
+from sparkparse.pages import home
+from sparkparse.styling import SitePalette, get_site_colors
+
+
+@callback(
+    [
+        Output("navbar-brand", "style"),
+    ],
+    [
+        Input("current-url", "pathname"),
+        Input("color-mode-switch", "value"),
+    ],
+)
+def update_link_color(pathname: str, dark_mode: bool):
+    _, color = get_site_colors(dark_mode, contrast=False)
+    highlighted_background_color, highlighted_text_color = get_site_colors(
+        dark_mode, contrast=True
+    )
+
+    pages = [""]
+    output_styles = [{"color": color} for _ in range(len(pages))]
+
+    if pathname in ["network"]:
+        return output_styles
+
+    current_page = pathname.removeprefix("/").split("-")[0]
+
+    output_styles[pages.index(current_page)] = {
+        "color": highlighted_text_color,
+        "backgroundColor": highlighted_background_color,
+        "borderRadius": "20px",
+    }
+
+    return output_styles
+
+
+@callback(
+    [
+        Output("color-mode-switch", "children"),
+        Output("color-mode-switch", "value"),
+    ],
+    Input("color-mode-switch", "n_clicks"),
+    State("color-mode-switch", "children"),
+)
+def toggle_color_mode(n_clicks, _):
+    is_dark = n_clicks % 2 == 1
+    if is_dark:
+        return html.I(
+            className="fas fa-sun",
+            style={"color": SitePalette.PAGE_BACKGROUND_COLOR_LIGHT},
+        ), True
+
+    return html.I(
+        className="fas fa-moon",
+        style={"color": SitePalette.PAGE_BACKGROUND_COLOR_DARK},
+    ), False
+
+
+@callback(
+    [
+        Output("sparkparse-page", "className"),
+        Output("navbar", "className"),
+    ],
+    Input("color-mode-switch", "value"),
+)
+def toggle_page_color(dark_mode: bool):
+    class_name = "dark-mode" if dark_mode else "light-mode"
+    return class_name, class_name
+
+
+def layout():
+    navbar = dbc.Navbar(
+        dbc.Container(
+            [
+                dbc.NavbarBrand(
+                    "sparkparse", href="/", class_name="navbar-brand", id="navbar-brand"
+                ),
+                dbc.Collapse(
+                    dbc.Nav(
+                        [
+                            dcc.Location("current-url", refresh=False),
+                        ]
+                    )
+                ),
+                dbc.NavItem(
+                    dbc.Button(
+                        id="color-mode-switch",
+                        n_clicks=0,
+                        children=html.I(
+                            className="fas fa-moon",
+                            style={
+                                "color": SitePalette.PAGE_BACKGROUND_COLOR_DARK,
+                            },
+                        ),
+                        color="link",
+                    )
+                ),
+            ],
+            fluid=True,
+            id="navbar-container",
+        ),
+        id="navbar",
+    )
+
+    return dbc.Container(
+        id="sparkparse-page",
+        children=[
+            navbar,
+            html.Br(),
+            dash.page_container,
+        ],
+        fluid=True,
+    )
+
+
+def init_dashboard(df: pl.DataFrame) -> dash.Dash:
+    app = dash.Dash(
+        __name__,
+        use_pages=True,
+        external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME],
+        suppress_callback_exceptions=True,
+    )
+
+    if df is not None:
+        app.df = df  # type: ignore
+
+    app.index_string = f"""
+    <!DOCTYPE html>
+    <html>
+        <head>
+            {{%metas%}}
+            <title>{{%title%}}</title>
+            {{%favicon%}}
+            {{%css%}}
+            <style>
+                body, #navbar {{
+                    background-color: {SitePalette.PAGE_BACKGROUND_COLOR_LIGHT} !important;
+                    margin: 0;
+                }}
+                
+            </style>
+        </head>
+        <body>
+            {{%app_entry%}}
+            <footer>
+                {{%config%}}
+                {{%scripts%}}
+                {{%renderer%}}
+            </footer>
+        </body>
+    </html>
+    """
+
+    app.layout = layout()
+    dash.register_page(home.__name__, name="summary", path="/", layout=home.layout)
+    return app
+
+
+def run_app(app):
+    app.run(host="127.0.0.1", debug=True)
---
+++ sparkparse/pages/home.py
@@ -0,0 +1,142 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_table
+import pandas as pd
+import plotly.express as px
+import polars as pl
+from dash import Input, Output, callback, dcc, html
+from plotly.graph_objs import Figure
+
+from sparkparse.common import timeit
+from sparkparse.styling import get_dt_style
+from sparkparse.viz import style_fig
+
+
+@callback(
+    [
+        Output("metrics", "figure"),
+        Output("metrics", "style"),
+        Output("metrics-graph-fade", "is_in"),
+    ],
+    [
+        Input("metrics-df", "data"),
+        Input("color-mode-switch", "value"),
+    ],
+)
+@timeit
+def get_metrics_viz(
+    df_data: list[dict],
+    dark_mode: bool,
+) -> tuple[Figure, dict, bool]:
+    if dark_mode:
+        template = "plotly_dark"
+    else:
+        template = "plotly"
+
+    df = pd.DataFrame(df_data).sort_values("task_id")
+    df["cumulative_runtime"] = df["executor_run_time"].cumsum()
+
+    title = f"<b>Cumulative Task Runtime</b><br><sup>{df['log_name'].iloc[0]}</sup>"
+
+    fig = px.scatter(
+        df,
+        x="task_id",
+        y="cumulative_runtime",
+        color="task_type",
+        title=title,
+        template=template,
+    )
+    x_min = df["task_id"].min()
+    x_max = df["task_id"].max()
+
+    fig = style_fig(fig, dark_mode, x_min, x_max)
+    return fig, {}, True
+
+
+@callback(
+    [
+        Output("metrics-table", "children"),
+        Output("metrics-table", "style"),
+    ],
+    [
+        Input("metrics-df", "data"),
+        Input("color-mode-switch", "value"),
+    ],
+)
+def get_styled_metrics_table(df_data: list[dict], dark_mode: bool):
+    df = pd.DataFrame(df_data)
+    metrics_style = get_dt_style(dark_mode)
+    metrics_style["style_table"]["height"] = "60vh"
+    tbl_cols = []
+    # float_style = {"type": "numeric", "format": {"specifier": ".1f"}}
+    int_style = {"type": "numeric", "format": {"specifier": ",d"}}
+
+    core_cols = [
+        "job_id",
+        "stage_id",
+        "task_id",
+        "task_type",
+        "executor_run_time",
+        "result_size",
+        "records_read",
+        "records_written",
+        "bytes_read",
+        "bytes_written",
+        "shuffle_write_time",
+        "shuffle_bytes_written",
+        "memory_bytes_spilled",
+        "disk_bytes_spilled",
+    ]
+
+    col_mapping = {col: int_style for col in core_cols if col in df.columns}
+    width_mapping = {col: 150 for col in core_cols if col in df.columns}
+    width_adjustment = [
+        {
+            "if": {"column_id": i},
+            "minWidth": width_mapping[i],
+            "maxWidth": width_mapping[i],
+        }
+        for i in width_mapping
+    ]
+    metrics_style["style_cell_conditional"].extend(width_adjustment)
+    core_df = df[core_cols]
+    for col in core_df.columns:
+        tbl_cols.append({**col_mapping[col], "id": col, "name": col.replace("_", " ")})
+
+    core_records = core_df.to_dict("records")
+
+    tbl = dash_table.DataTable(
+        core_records,
+        columns=tbl_cols,
+        **metrics_style,
+    )
+
+    return tbl, {}
+
+
+def layout():
+    if not hasattr(dash.get_app(), "df"):
+        return html.Div("No data loaded")
+
+    df: pl.DataFrame = dash.get_app().df
+    records = df.to_pandas().to_dict("records")
+
+    return [
+        dcc.Store("metrics-df", data=records),
+        dbc.Fade(
+            id="metrics-graph-fade",
+            children=[
+                dcc.Graph(
+                    "metrics",
+                    style={"visibility": "hidden"},
+                    config={"displayModeBar": False},
+                ),
+                html.Div(
+                    id="metrics-table",
+                    style={"visibility": "hidden"},
+                ),
+            ],
+            style={"transition": "opacity 200ms ease-in", "minHeight": "100vh"},
+            is_in=False,
+        ),
+    ]
