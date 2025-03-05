b9e337d5cf4829a88678305301f1cdc6158db908
Jeff Brennan <jeffbrennan10@gmail.com>
2025-03-05 00:31:16 -0500

    add broadcast joint test

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
 from pathlib import Path
-from sparkparse.app import get_spark
+
 import pyspark.sql.functions as F
+
+from sparkparse.common import get_spark
 
 
 def config():
@@ -10,6 +12,20 @@ def config():
     spark = get_spark(log_dir)
 
     return spark, data_path, base_dir
+
+
+def test_broadcast_join():
+    spark, data_path, base_dir = config()
+    df = spark.read.parquet(data_path.as_posix())
+    base_df = df.select("id1", "v3")
+    broadcast_df = df.limit(10).select("id1", "id2")
+    df_after_broadcast = base_df.join(broadcast_df, how="left", on="id1")
+    df_final = (
+        df_after_broadcast.groupBy("id2").agg(F.sum("v3").alias("v3")).orderBy("v3")
+    )
+
+    output_path = base_dir / "clean" / "broadcast"
+    df_final.write.format("csv").mode("overwrite").save(str(output_path), header=True)
 
 
 def test_row_count_explosion_join():
